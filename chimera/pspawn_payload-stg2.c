7de8925e4b04ddc2ae7663e068116236872cbff1
Jake James <jakeashacks@gmail.com>
2019-05-02 20:41:06 +0200

    done stage2

--- chimera/pspawn_payload-stg2.c
+++ chimera/pspawn_payload-stg2.c
@@ -35,6 +35,9 @@ enum sandbox_filter_type {
 extern const enum sandbox_filter_type SANDBOX_CHECK_NO_REPORT __attribute__((weak_import));
 int sandbox_check(pid_t pid, const char *operation, enum sandbox_filter_type type, ...);
 
+int jbd_file_op(mach_port_t port, char a2, uint64_t a3, char *path, unsigned int len);
+signed long long jbd_call(mach_port_t port, char a2, int a3);
+
 int (*old_fcntl)(int fd, int b);
 int fake_fcntl(int fd, int b) {
     if (b == F_ADDFILESIGS_RETURN) return -1;
@@ -104,11 +107,223 @@ uint64_t fake_hasCdHash(uint64_t a, uint64_t b) {
 mach_port_t jbd_port;
 kern_return_t bootstrap_look_up(mach_port_t  bootstrap_port, char *service_name, mach_port_t *service_port);
 
+// something is LIKELY wrong but idc that much
 int (*old_pspawn)(pid_t *restrict, const char *restrict, const posix_spawn_file_actions_t *, const posix_spawnattr_t *restrict, char *const *restrict __argv, char *const *restrict __envp);
 int fake_pspawn(pid_t *restrict pid , const char *restrict path, const posix_spawn_file_actions_t *actions, const posix_spawnattr_t *restrict attr, char *const *restrict argv, char *const *restrict envp) {
     
-    // eta son, this part is long but basically DYLD_INSERT_LIBRARIES & jbd
-    return 0;
+    if (!jbd_port && bootstrap_look_up(bootstrap_port, "org.coolstar.jailbreakd", (mach_port_t *)&jbd_port)) {
+        return old_pspawn(pid, path, actions, attr, argv, envp);
+    }
+    
+    size_t len, size, sz;
+    char **arr;
+    signed int i;
+    int slen;
+    unsigned int num1, num2;
+    int num3;
+    
+    char *stg2 = strdup("/usr/lib/pspawn_payload-stg2.dylib");
+    struct stat st;
+    if (!stg2 || stat(stg2, (struct stat *)&st)) {
+        return old_pspawn(pid, path, actions, attr, argv, envp);
+    }
+    
+    char **envp2, **envp3;
+    char *env, *envar;
+    if (envp && (env = *envp) != 0) {
+        
+        num1 = 0;
+        num3 = 0;
+        envp2 = (char **)envp;
+        
+        do {
+            if (strstr(env, "DYLD_INSERT_LIBRARIES=")) {
+                free(stg2);
+                
+                char *str = strdup(*envp + 22);
+                char ch = *str;
+                
+                num2 = num1;
+                envp3 = envp2;
+                
+                if (ch) {
+                    size_t l = strlen(str);
+                    if (l <= 1) len = 1;
+                    else len = l;
+                    size = (8 * len + 8) & 0x7fffffff8;
+                }
+                else {
+                    size = 8;
+                }
+                
+                arr = (char **)malloc(size);
+                bzero(arr, size);
+                
+                char *tok = strtok(str, ":");
+                if (tok) {
+                    i = 0;
+                    slen = 0;
+                    
+                    do {
+                        if (strcmp(tok, "/usr/lib/pspawn_payload-stg2.dylib")) {
+                            arr[i] = strdup(tok);
+                            slen += strlen(tok);
+                            i++;
+                        }
+                        tok = strtok(NULL, ":");
+                    }
+                    while (tok);
+                }
+                else {
+                    slen = 0;
+                    i = 0;
+                }
+                free(str);
+                
+                sz = slen + i + 34 + 1;
+                stg2 = (char *)malloc(sz);
+                bzero(stg2, sz);
+                strncpy(stg2, "/usr/lib/pspawn_payload-stg2.dylib", 34);
+                
+                if (i >= 1) {
+                    int i2 = i;
+                    char **arr2 = arr;
+                    size_t pos = 34;
+                    
+                    do {
+                        strncpy(&stg2[pos], ":", 1);
+                        
+                        char *s = &stg2[pos + 1];
+                        char *d = *arr2;
+                        size_t l = strlen(d);
+                        strncpy(s, d, l);
+                        d = *arr2;
+                        arr2++;
+                        pos = strlen(d) + pos + 1;
+                        free(d);
+                        i2--;
+                    } while (i2);
+                }
+                free(arr);
+                num1 = num2;
+                envp2 = envp3;
+            }
+            else num3++;
+            num1++;
+            envar = envp2[1];
+            envp2++;
+            env = envar;
+        }
+        while (envar);
+    }
+    else {
+        num1 = 0;
+        num3 = 0;
+    }
+    
+    char **buf = (char **)malloc((signed long long)((uint64_t)(unsigned int)(num3 + 2) << 32) >> 29);
+    int idx = 0;
+    size_t num;
+    
+    if (num1) {
+        num = num1;
+        do {
+            char *en = *envp;
+            if (!strstr(*envp, "DYLD_INSERT_LIBRARIES")) buf[idx++] = en;
+            envp++;
+            num--;
+        }
+        while (num);
+    }
+    
+    size_t l = strlen(stg2);
+    char *new = (char *)malloc(l + 23);
+    *new = 0;
+    strcat(new, "DYLD_INSERT_LIBRARIES=");
+    strcat(new, stg2);
+    free(stg2);
+    
+    char **arr_ = &buf[idx];
+    *arr_ = new;
+    arr_[1] = NULL;
+    
+    short flags;
+    
+    posix_spawnattr_t newattr;
+    if (attr) {
+        posix_spawnattr_getflags((posix_spawnattr_t *)attr, &flags);
+        flags |= POSIX_SPAWN_START_SUSPENDED;
+        posix_spawnattr_setflags((posix_spawnattr_t *)attr, flags);
+        flags = 0;
+        posix_spawnattr_getflags((posix_spawnattr_t *)attr, &flags);
+    }
+    else {
+        posix_spawnattr_init(&newattr);
+        posix_spawnattr_setflags(&newattr, POSIX_SPAWN_START_SUSPENDED);
+        flags = 0;
+    }
+    
+    char real[MAXPATHLEN];
+    int ret;
+    
+    if (stat(path, (struct stat *)&st) != -1) {
+        bzero(real, MAXPATHLEN);
+        realpath(path, real);
+        
+        ret = jbd_file_op(jbd_port, 1, st.st_ino, real, (unsigned int)strlen(real));
+        if ((unsigned int)(ret - 0x10000003) > 1) {
+            if (!ret) {
+            loop:;
+                while (1) {
+                    mach_port_t jbd = *((mach_port_t *)&NDR_record + 202);
+                    ret = jbd_file_op(jbd, 2, st.st_ino, real, (unsigned int)strlen(real));
+                    if (!ret) {
+                        break;
+                    }
+                    usleep(0x64);
+                }
+                goto label;
+            }
+            // assume this is os_log: NSLog(@"%d Unknown return value for preflight: 0x%x", smth, smth);
+        }
+        else {
+            jbd_port = 0;
+            bootstrap_look_up(bootstrap_port, "org.coolstar.jailbreakd", (mach_port_t *)&jbd_port);
+            ret = jbd_file_op(jbd_port, 1, st.st_ino, real, (unsigned int)strlen(real));
+        }
+        if (ret) {
+            goto label;
+        }
+        goto loop;
+    }
+label:;
+    if (flags & POSIX_SPAWN_SETEXEC) {
+        if ((unsigned int)jbd_call(jbd_port, 3, getpid()) - 0x10000003 <= 1) {
+            jbd_port = 0;
+            bootstrap_look_up(bootstrap_port, "org.coolstar.jailbreakd", (mach_port_t *)&jbd_port);
+            jbd_call(jbd_port, 3, getpid());
+        }
+        ret = old_pspawn(pid, path, actions, newattr, argv, buf);
+        free(new);
+        free(buf);
+    }
+    else {
+        int p;
+        ret = old_pspawn(&p, path, actions, newattr, argv, buf);
+        free(new);
+        free(buf);
+        
+        if (pid) {
+            *pid = p;
+        }
+        
+        if ((unsigned int)jbd_call(jbd_port, 2, p) - 268435459 <= 1) {
+            jbd_port = 0;
+            bootstrap_look_up(bootstrap_port, "org.coolstar.jailbreakd", (mach_port_t *)&jbd_port);
+            jbd_call(jbd_port, 3, p);
+        }
+    }
+    return ret;
 }
 
 int (*old_execve)(const char *__file, char *const *__argv, char *const *__envp);
